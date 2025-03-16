e86ee233cb31300ad447ab8127600fbd6901ccd5
StatguyUser <azimulh@gmail.com>
2025-03-16 12:31:25 +0530

    updated files

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ else:
 
 setup(
     name='AxisLabeling',
-    version='0.0.1',
+    version='0.0.2',
     description='Implementation of axis-labeling algorithms',
     long_description=long_description,
     long_description_content_type='text/markdown',  # This is important!
     author='StatguyUser',
     url='https://github.com/StatguyUser/AxisLabeling',
-    install_requires=['math','numpy'],
+    install_requires=['numpy'],
     download_url='https://github.com/StatguyUser/AxisLabeling.git',
     py_modules=["AxisLabeling"],
     package_dir={'':'src'},
